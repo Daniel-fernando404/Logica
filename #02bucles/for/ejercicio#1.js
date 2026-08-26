de47b2a1ed8dd2b581550d60c5fad28aa2df0d73
Fernando <fernandany34@gmail.com>
2026-08-26 19:21:13 -0300

    creacion del ejercicio 1 de for

---
+++ #02bucles/for/ejercicio#1.js
@@ -0,0 +1,16 @@
+/*
+Escenario:
+Estás programando la secuencia de lanzamiento de un cohete espacial.
+
+Reglas de negocio:
+
+El lanzamiento comienza en el segundo 10 y desciende segundo a segundo hasta llegar a 0.
+
+Implementa la cuenta usando un bucle for.
+
+En cada segundo, debe imprimir en consola: "T-minus [SEGUNDO] segundos".
+
+Cuando el contador llegue a 5, debe imprimir una advertencia especial: "T-minus 5 segundos: Motores principales encendidos".
+
+Al llegar a 0, el sistema debe imprimir el mensaje de despegue final: "¡Despegue exitoso!".
+*/

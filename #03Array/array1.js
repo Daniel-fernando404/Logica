2315e7dbd35b7f60fe0488311b561bd9da621878
Fernando <fernandany34@gmail.com>
2026-09-01 21:46:22 -0300

    Ejercicio 1 array

---
+++ #03Array/array1.js
@@ -0,0 +1,23 @@
+/*
+Escenario:
+Vas a crear un registro de notas de un estudiante y consultar sus valores.
+
+Reglas de negocio:
+
+Crea un array llamado notas con cuatro calificaciones: 8, 6, 10 y 7.
+
+Imprime en consola la primera nota de la lista usando su posición.
+
+Imprime en consola la última nota de la lista usando su posición.
+
+Imprime en consola la cantidad total de notas usando .length.
+*/
+
+let notas = [8,6,10,7]
+
+console.log("Primera nota" + " " + notas[0])
+
+console.log("Ultima nota" + " "+  notas[3])
+
+//notas.length
+console.log("Cantidad total de notas: " + notas.length)
